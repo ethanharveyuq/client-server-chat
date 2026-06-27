1832a7ee8c0c6ec563bb4ff6956cc017613e67e7
Ethan Harvey <ethanjharvey06@gmail.com>
2026-06-27 20:39:39 +1000

    Changed server class structure and added listen + accept logic

--- include/server.hpp
+++ include/server.hpp
@@ -1,19 +1,22 @@
 #include <vector>
 #include <cerrno>
 #include <sys/socket.h>
-#include <iostream>
 #include <netinet/in.h>
 #include <unistd.h>
 #include <cstring>
 
 class Server {
-    int listen_fd;
+    int server_fd;
+    int limit;
+    bool running;
     std::vector<int> client_fds;
     std::mutex clients_mutex;
     struct sockaddr_in server_addr;
+    int open_server(int port);
 public:
-    Server(int port = 0);
+    Server(int lim, int port = 0);
     ~Server();
+    void listen_accept();
     void broadcast(const std::string& msg);
     void handle_client(int fd);
 };
--- src/server.cpp
+++ src/server.cpp
@@ -2,18 +2,27 @@
 #include <sys/socket.h>
 #include <iostream>
 #include <netinet/in.h>
+#include <thread>
 #include <unistd.h>
 #include <cstring>
 #include "../include/server.hpp"
 
 
-Server::Server(int port) {
-        // initialise socket
-    listen_fd = socket(AF_INET, SOCK_STREAM, 0); // TCP and IPv4
-    if (listen_fd < 0) {
+Server::Server(int lim, int port) {
+    limit = lim;
+    running = false;
+    client_fds = {};
+    // socket and bind
+    open_server(port);
+}
+
+int Server::open_server(int port) {
+    // initialise socket
+    server_fd = socket(AF_INET, SOCK_STREAM, 0); // TCP and IPv4
+    if (server_fd < 0) {
         // handle error
         std::cerr << "Socket creation failed\n";
-        return;
+        return -1;
     }
 
     server_addr = sockaddr_in{};
@@ -22,22 +31,45 @@ Server::Server(int port) {
     server_addr.sin_port = htons(port);   // port chosen (default 0)
     server_addr.sin_addr.s_addr = INADDR_ANY;  // Bind to all available network interfaces
 
-    if (bind(listen_fd, (struct sockaddr*) &server_addr, sizeof(struct sockaddr)) < 0) {
+    if (bind(server_fd, (struct sockaddr*) &server_addr, sizeof(struct sockaddr)) < 0) {
         // socket bind failed, reason in errno
         std::cerr << "Socket bind failed\n";
         int serrno = errno; // save errno for caller
-        close(listen_fd);
+        close(server_fd);
         errno = serrno;
-        return;     
+        return -1;     
     }
+
+    return 0;
 }
 
 // Delete server functionality
 Server::~Server() {
-    if (listen_fd >= 0) {
-        close(listen_fd);
+    if (server_fd >= 0) {
+        close(server_fd);
     }
     // will clear the client_fds and mutex automatically (won't use new)
+}
+
+void Server::listen_accept() {
+    running = true;
+    int status = listen(server_fd, limit);
+    if (status < 0) {
+        std::cerr << "Server listening failed\n";
+        return;
+    }
+
+    while (running) {
+        int client_fd = accept(server_fd, nullptr, nullptr);
+
+        if (client_fd < 0) {
+            std::cerr << "Client accept failed\n";
+            return;
+        }
+        client_fds.push_back(client_fd);
+
+        std::thread(&Server::handle_client, this, client_fd).detach(); // detach for now, will use ctrl+C to end
+    }
 }
 
 void Server::broadcast(const std::string& msg) {
